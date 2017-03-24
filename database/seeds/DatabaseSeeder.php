3a35517cb2d77a9cfb7d345e367e6beea627f89f
ccckblaze <ccckblaze@sina.com>
2017-03-24 13:38:32 +0800

    Init

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder {
+
+    public function run()
+    {
+        $this->call('UserTableSeeder');
+
+        $this->command->info('User table seeded!');
+    }
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        App\User::create(['email' => 'foo@bar.com', 'password' => 'asdfasdffd']);
+    }
+
+}
